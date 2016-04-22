010b4eda12f261484da51578661888f85da9ef8a
Helton Carlos de Souza <helton.development@gmail.com>
2016-04-22 00:26:05 -0300

    Adding ng-repeat example

---
+++ get-started/angular-get-started-09-ng-repeat/script.js
@@ -0,0 +1,32 @@
+(function() {
+  var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+        .then(onRepos, onError);    
+      $scope.error = null;
+    };
+    
+    var onRepos = function(response) {
+      $scope.repos = response.data
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the data.";
+    };
+
+    $scope.search = function(username) {
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);
+    };    
+
+    $scope.username = "angular";
+    $scope.message = "GitHub Viewer";
+
+  };
+  
+  var app = angular.module("githubViewer", []);
+  app.controller("MainController", MainController);
+
+})();
