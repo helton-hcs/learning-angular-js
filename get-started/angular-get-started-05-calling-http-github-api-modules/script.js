0bb49ce67c8ade8073fd8859206af4c7aa60eac8
Helton Carlos de Souza <helton.development@gmail.com>
2016-04-21 22:38:54 -0300

    Adding modules example

---
+++ get-started/angular-get-started-05-calling-http-github-api-modules/script.js
@@ -0,0 +1,22 @@
+(function() {
+  var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the user";
+    };
+
+    $http.get("https://api.github.com/users/torvalds")
+      .then(onUserComplete, onError);
+
+    $scope.message = "Hello, Angular!";
+
+  };
+  
+  var app = angular.module("githubViewer", []);
+  app.controller("MainController", MainController);
+
+})();
